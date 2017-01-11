eee9539a194227c27aea6142b5ee54821cf0cf68
Víctor Colombo <victorcora98@gmail.com>
2017-01-11 14:54:35 -0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,19 @@ function displayTime()
 {
   var l = document.getElementById("localClock");
   var u = document.getElementById("UTCClock");
+  var s = document.getElementById("selectedClock");
+  var dropdown = document.getElementById("input");
+  var optionValue = dropdown.options[dropdown.selectedIndex].value;
   var now = new Date();
   l.innerHTML = now.toString();
   u.innerHTML = now.toUTCString();
+  if(optionValue)
+{
+  optionValue = -optionValue;
+  now.setHours(now.getUTCHours() - optionValue);
+}
+  //else { now.setHours(now.getUTCHours() + optionValue); }
+  s.innerHTML = now.toString();
   setTimeout(displayTime, 1000);
 }
 
